cbeb5bbb915c44e22c549dddba4b6ade7ab971da
EMILYVTHOMAS1 <emilyvthomas.evt@gmail.com>
2018-11-05 14:30:45 +0530

    Added file_uploading file to shop_admin

---
+++ shop_admin/file_up.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>File Upload</h1>
+        <br/>
+        <table>
+            <tr>
+                <td>my Photo</td>
+                <td><input name="f1" type="submit"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit"</td>
+            </tr>
+        </table>
+    </body>
+    
+</html>
+
