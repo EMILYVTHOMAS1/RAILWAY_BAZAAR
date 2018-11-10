7d2dbe8316b7a1de19dd966ba3f9376449fa75d6
chaithanya5991 <42314356+chaithanya5991@users.noreply.github.com>
2018-11-10 22:20:59 +0530

    Updated registration for shop
    Registration page for new shops has been updated successfully with more validations.

--- org.php
+++ org.php
@@ -13,13 +13,13 @@ if(isset($_POST['shop']))
     $shopp=$_POST['shopp'];
     $shopuid=$_POST['shopuid'];
     $ac=$_POST['ac'];
-    $sel4=  mysql_query("select * from shop_reg");
-    $count=  mysql_num_rows($sel4);
+    $sel4=  mysqli_query($$con,"select * from shop_reg");
+    $count=  mysqli_num_rows($sel4);
     $count1=$count+1;
 $npic=$count1."".substr($shop_pic,strrpos($shop_pic,"."));
 move_uploaded_file($_FILES['shop_pic']['tmp_name'],getcwd()."\\shop\\$npic");
-$sel24=  mysql_query("select * from bank where acnt='$ac'");
-if(mysql_num_rows($sel24)>0)
+$sel24=  mysqli_query($con,"select * from bank where acnt='$ac'");
+if(mysqli_num_rows($sel24)>0)
 {
    $shop_cont=$_POST['shop_cont'];
      $item="$shop_cont";
@@ -29,7 +29,7 @@ if(mysql_num_rows($sel24)>0)
                 win.close()
             }, 6000);</script>";
   
-   $ins3=  mysql_query("insert into shop_reg values('','$shop_num','$shop_nme','$shop_owner','$shop_cat','$shop_ns','$npic','$shop_cont','$shopuid','$shopp','shop','0','$ac','0') "); 
+   $ins3=  mysqli_query($con,"insert into shop_reg values('','$shop_num','$shop_nme','$shop_owner','$shop_cat','$shop_ns','$npic','$shop_cont','$shopuid','$shopp','shop','0','$ac','0') "); 
    
 }
 
@@ -47,7 +47,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html>
 <head>
-<title>railwaybazar</title>
+<title>Railway Bazaar</title>
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -63,6 +63,70 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- web-fonts -->
 
 <!-- //web-fonts --> 
+<script type="text/javascript">
+    
+    
+    function chkc(b2)
+{
+var k5=b2.length;
+var ch2=/([0-9])$/;
+if(ch2.test(b2)==false)
+{
+document.getElementById("o3").innerHTML="<font color='red'>*Only Numbers*</font>";
+$("#btn").hide();
+return false;
+}
+else if(k5!=10)
+{
+document.getElementById("o3").innerHTML="<font color='red'>*Please Check Your Mobile Number*</font>";
+$("#btn").hide();
+return false;
+}
+else
+{
+  document.getElementById("o3").innerHTML="";  
+  $("#btn").show();
+}
+}
+  
+ function chkp(c)
+{
+var s=document.getElementById("p10").value;
+
+if(s==c)
+{
+document.getElementById("p").innerHTML="<font color='Green'>*Password is Correct*</font>";
+$("#btn").show();
+return false;
+}
+else
+{
+document.getElementById("p").innerHTML="<font color='red'>*Verfy Password*</font>";
+$("#btn").hide();
+}
+}
+
+
+
+
+
+function validateemail(a)  
+     {  
+          //var a = document.myform.email.value;  
+          var atposition = a.indexOf("@");  
+          var dotposition = a.lastIndexOf(".");  
+          if (atposition<1 || dotposition<atposition+2 || dotposition+2>=a.length) 
+          {  
+               document.getElementById("em").innerHTML="<font color='red'>*Please Check Your Email Address*</font>";
+                $("#btn").hide();  
+          }  
+          else
+{
+                document.getElementById("em").innerHTML="";  
+  $("#btn").show();
+}
+     }  
+    </script>
 </head>
 <body>
 	<!-- banner --> 
@@ -70,7 +134,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<div class="center-container">
 	    <div class="w3ls-agileinfo">
 		
-          <h1> RAILWAY_BAZAAR</h1>	
+          <h1> RAILWAY BAZAAR </h1>	
 		  </div>
 		 <div class="bg-agile">
 		 <div class="top-icons-agileits-w3layouts">
@@ -86,19 +150,19 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 				<li>Shop</li>
 			 </ul>
 		 </div>
-			<h2> </h2>
-			<h3></h3>
+                     <br>
+			<h3>Shop registration</h3>
 			<div class="login-form">			
 							<form method="post" enctype="multipart/form-data" name="myForm" onsubmit="return validateForm()">
                                                             <input placeholder="Shop Number" type="text" name="shop_num" required="required" />
-                                                            <input type="text"placeholder="Shop Name" name="shop_nme" required="required" />
+                                                            <input type="text"placeholder="Shop Number" name="shop_nme" required="required" />
                                                             <input type="text"Placeholder="Ownership Name" name="shop_owner" required="required"/>
                                                             
                                                             <select name="shop_cat" class="form-control" required="required"> 
                                                                   <option value="-1">--choose one--</option>
                                                                   <?php
-                                                                  $sel30=  mysql_query("select * from add_cat");
-                                                                  while ($r21=  mysql_fetch_row($sel30))
+                                                                  $sel30=  mysqli_query($con,"select * from add_cat");
+                                                                  while ($r21=  mysqli_fetch_row($sel30))
                                                                   {
                                                                       ?>
                                                                   <option value="<?php echo $r21[2]  ?>"><?php echo $r21[2]  ?></option>
@@ -110,8 +174,8 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
                                                             <select name="shop_ns" class="form-control" required="required" > 
                                                                      <option>---choose one---</option>
                                                                       <?php
-                                                                  $sel30=  mysql_query("select * from train_mangmt");
-                                                                  while ($r21=  mysql_fetch_row($sel30))
+                                                                  $sel30=  mysqli_query($con,"select * from train_mangmt");
+                                                                  while ($r21=  mysqli_fetch_row($sel30))
                                                                   {
                                                                       ?>
                                                                   <option value="<?php echo $r21[0] ?>"><?php echo $r21[4]."-".$r21[5]   ?></option>
@@ -122,9 +186,9 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
                                                               </select>
                                                             
                                                             <input type="file" name="shop_pic" required="required"/>
-                                                            <input type="number"placeholder="Contact Number" name="shop_cont" maxlength="10" required="required" class="form-control"/>
+                                                            <input type="number"placeholder="Contact Number" name="shop_cont" maxlength="10" required="required" class="form-control"onblur="chkc(this.value)" /><span id="o3"></span>
                                                             <input placeholder="Username" type="text" name="shopuid" required="required" />
-                                                            <input type="password"placeholder="Password" name="shopp" required="required" class="form-control"/>
+                                                            <input type="password"placeholder="Password" name="shopp" class="form-control" required="required"/>
                                                             <input type="text"placeholder="Your Account Number" name="ac" required="required"/>
                                                             <input name="shop" type="submit" value="Submit">
 							</form>	
