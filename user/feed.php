9abb58374db037a228cbdfed3a4285f9e396a8d0
chaithanya5991 <42314356+chaithanya5991@users.noreply.github.com>
2018-11-11 23:01:08 +0530

    Implemented feedback for user module
    Feedback implemented successfully for user to post feedbacks regarding the products purchased.

---
+++ user/feed.php
@@ -0,0 +1,412 @@
+<?php
+include("../connection.php");
+session_start();
+$user_name=$_SESSION['user'];
+ if($user_name=$_SESSION['user'])
+ {
+     
+ }
+ else {
+header("location:index.php");     
+}
+
+?>
+ <?php
+                if(isset($_POST['fd']))
+                {
+                    $abt=$_POST['abt'];
+                    $shop=$_POST['shop_ns'];
+                    $st=$_POST['fdbk'];
+                    $ins5=  mysqli_query($con,"insert into post_feedback values('','$user_name','$abt','$shop','$st')");
+                }
+                ?>
+
+
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Big store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | Contact :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Vide" />
+<meta name="keywords" content="Big store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="../temp/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="../temp/css/style.css" rel='stylesheet' type='text/css' />
+<!-- js -->
+   <script src="../temp/js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="../temp/js/move-top.js"></script>
+<script type="text/javascript" src="../temp/js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<link href="../temp/css/font-awesome.css" rel="stylesheet"> 
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+<!--- start-rate---->
+<script src="../temp/js/jstarbox.js"></script>
+	<link rel="stylesheet" href="../temp/css/jstarbox.css" type="text/css" media="screen" charset="utf-8" />
+		<script type="text/javascript">
+			jQuery(function() {
+			jQuery('.starbox').each(function() {
+				var starbox = jQuery(this);
+					starbox.starbox({
+					average: starbox.attr('data-start-value'),
+					changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
+					ghosting: starbox.hasClass('ghosting'),
+					autoUpdateAverage: starbox.hasClass('autoupdate'),
+					buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
+					stars: starbox.attr('data-star-count') || 5
+					}).bind('starbox-value-changed', function(event, value) {
+					if(starbox.hasClass('random')) {
+					var val = Math.random();
+					starbox.next().text(' '+val);
+					return val;
+					} 
+				})
+			});
+		});
+		</script>
+<!---//End-rate---->
+
+</head>
+<body><?php
+    
+        include 'menu.php';
+    ?>
+ <!--banner-->
+<div class="banner-top">
+	<div class="container">
+		<h3 >Contact</h3>
+		<h4><a href="../temp/index.html">Home</a><label>/</label>Contact</h4>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+
+<!-- contact -->
+<div class="contact">
+	<div class="container">
+		<div class="spec ">
+			<h3>Post your Feedback</h3>
+				<div class="ser-t">
+					<b></b>
+					<span><i></i></span>
+					<b class="line"></b>
+				</div>
+		</div>
+		<div class=" contact-w3">	
+			<div class="col-md-5 contact-right">	
+                            <img style="height: 570px" src="../temp/images/send-email.gif" class="img-responsive" alt="">
+				</div>
+			<div class="col-md-7 contact-left">
+				<h4>Contact Information</h4>
+				
+				<ul class="contact-list">
+					<li> <i class="fa fa-map-marker" aria-hidden="true"></i> 756 Global Place, New York.</li>
+					<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="../temp/mailto:example@mail.com">mail@example.com</a></li>
+					<li> <i class="fa fa-phone" aria-hidden="true"></i>+123 2222 222</li>
+				</ul>
+				<div id="container">
+					<!--Horizontal Tab-->
+					<div id="parentHorizontalTab">
+						<ul class="resp-tabs-list hor_1">
+							<li><i class="fa fa-envelope" aria-hidden="true"></i></li>
+							<li> <i class="fa fa-map-marker" aria-hidden="true"></i> </span></li>
+							<li> <i class="fa fa-phone" aria-hidden="true"></i></li>
+						</ul>
+						<div class="resp-tabs-container hor_1">
+							<div>
+								<!--<form  method="post">-->
+                                                                <form method="post">
+									
+                                                                    <select name="abt" class="form-control">
+                                <option value="-1">
+                                    ---choose one---
+                                </option>
+                                <?php
+                                $sel10=mysqli_query($con,"select * from shop_reg where type='shop'");
+                                if(mysqli_num_rows($sel10)>0)
+                                {
+                                    while ($r5=  mysqli_fetch_row($sel10))
+                                    {
+                                        ?>
+                                <option value="<?php echo $r5[2] ?>"><?php echo $r5[2] ?></option>
+                                
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </select>
+                                        <br/>                            
+                                                                    
+                                                                   <select name="shop_ns" class="form-control" > 
+                               
+                                         <option value="-1">
+                                    ---choose one---
+                                </option>                               <?php
+                                $sel10=mysqli_query($con,"select * from train_mangmt");
+                                if(mysqli_num_rows($sel10)>0)
+                                {
+                                    while ($r5=  mysqli_fetch_row($sel10))
+                                    {
+                                        ?>
+                                <option value="<?php echo $r5[4] ?>"><?php echo $r5[4] ?></option>
+                                
+                                <?php
+                                    }
+                                }
+                                ?>
+                           </select>
+                                        <br/>
+                                                                    
+                                                                    <td><textarea cols="5" rows="5" name="fdbk" class="form-control"> </textarea></td>
+                                                                   <input name="fd" type="submit" value="Submit" >
+								</form>
+							</div>
+							<div>
+								<div class="map-grid">
+								<h5>Our Branches</h5>
+									<ul>
+										<li><i class="fa fa-arrow-right" aria-hidden="true"></i>756 Global Place, New York.</li>
+										<li><i class="fa fa-arrow-right" aria-hidden="true"></i>889 diamond street, USA.</li>
+										<li><i class="fa fa-arrow-right" aria-hidden="true"></i>756 global Place, New York.</li>
+										<li><i class="fa fa-arrow-right" aria-hidden="true"></i>889 diamond street, USA.</li>
+									</ul>
+								</div>
+							</div>
+							<div>
+								<div class="map-grid">
+									<h5>Contact Me Through</h5>
+									<ul>
+										<li>Mobile No : +123 456 7890</li>
+										<li>Office No : +123 222 2222</li>
+										<li>Home No : +123 456 7890</li>
+										<li>Fax No : +123 456 7890</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<!--Plug-in Initialisation-->
+				<script type="text/javascript">
+				$(document).ready(function() {
+					//Horizontal Tab
+					$('#parentHorizontalTab').easyResponsiveTabs({
+						type: 'default', //Types: default, vertical, accordion
+						width: 'auto', //auto or any width like 600px
+						fit: true, // 100% fit in a container
+						tabidentify: 'hor_1', // The tab groups identifier
+						activate: function(event) { // Callback function if tab is switched
+							var $tab = $(this);
+							var $info = $('#nested-tabInfo');
+							var $name = $('span', $info);
+							$name.text($tab.text());
+							$info.show();
+						}
+					});
+
+					// Child Tab
+					$('#ChildVerticalTab_1').easyResponsiveTabs({
+						type: 'vertical',
+						width: 'auto',
+						fit: true,
+						tabidentify: 'ver_1', // The tab groups identifier
+						activetab_bg: '#fff', // background color for active tabs in this group
+						inactive_bg: '#F5F5F5', // background color for inactive tabs in this group
+						active_border_color: '#c1c1c1', // border color for active tabs heads in this group
+						active_content_border_color: '#5AB1D0' // border color for active tabs contect in this group so that it matches the tab head border
+					});
+
+					//Vertical Tab
+					$('#parentVerticalTab').easyResponsiveTabs({
+						type: 'vertical', //Types: default, vertical, accordion
+						width: 'auto', //auto or any width like 600px
+						fit: true, // 100% fit in a container
+						closed: 'accordion', // Start closed if in accordion view
+						tabidentify: 'hor_1', // The tab groups identifier
+						activate: function(event) { // Callback function if tab is switched
+							var $tab = $(this);
+							var $info = $('#nested-tabInfo2');
+							var $name = $('span', $info);
+							$name.text($tab.text());
+							$info.show();
+						}
+					});
+				});
+			</script>
+				
+			</div>
+			
+		<div class="clearfix"></div>
+	</div>
+	</div>
+</div>
+<!-- //contact -->
+
+<!--footer-->
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 footer-grid">
+			<h3>About Us</h3>
+			<p>Nam libero tempore, cum soluta nobis est eligendi 
+				optio cumque nihil impedit quo minus id quod maxime 
+				placeat facere possimus.</p>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Menu</h3>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Kitchen</a></li>
+				<li><a href="../temp/care.html">Personal Care</a></li>
+				<li><a href="../temp/hold.html">Household</a></li>						 
+				<li><a href="../temp/codes.html">Short Codes</a></li> 
+				<li><a href="../temp/contact.html">Contact</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Customer Services</h3>
+			<ul>
+				<li><a href="../temp/shipping.html">Shipping</a></li>
+				<li><a href="../temp/terms.html">Terms & Conditions</a></li>
+				<li><a href="../temp/faqs.html">Faqs</a></li>
+				<li><a href="../temp/contact.html">Contact</a></li>
+				<li><a href="../temp/offer.html">Online Shopping</a></li>						 
+				 
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid">
+			<h3>My Account</h3>
+			<ul>
+				<li><a href="../temp/login.html">Login</a></li>
+				<li><a href="../temp/register.html">Register</a></li>
+				<li><a href="../temp/wishlist.html">Wishlist</a></li>
+				
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+			<div class="footer-bottom">
+				<h2 ><a href="../temp/index.html"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
+				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+				<ul class="social-fo">
+					<li><a href="../temp/#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="../temp/#" class=" twi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="../temp/#" class=" pin"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="../temp/#" class=" dri"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				</ul>
+				<div class=" address">
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
+					</div>
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>	
+					</div>
+					<div class="col-md-4 fo-grid1">
+						<p><a href="../temp/mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
+					</div>
+					<div class="clearfix"></div>
+					
+					</div>
+			</div>
+		<div class="copy-right">
+			<p> &copy; 2016 Big store. All Rights Reserved | Design by  <a href="../temp/http://w3layouts.com/"> W3layouts</a></p>
+		</div>
+	</div>
+</div>
+<!-- //footer-->
+<!-- tabs -->
+<script src="../temp/js/easyResponsiveTabs.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('#horizontalTab').easyResponsiveTabs({
+			type: 'default', //Types: default, vertical, accordion           
+			width: 'auto', //auto or any width like 600px
+			fit: true   // 100% fit in a container
+			});
+		});				
+	</script>
+<!-- //tabs -->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="../temp/#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<!-- for bootstrap working -->
+		<script src="../temp/js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script type='text/javascript' src="../temp/js/jquery.mycart.js"></script>
+  <script type="text/javascript">
+  $(function () {
+
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="../temp/' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+
+    $('.my-cart-btn').myCart({
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) {
+        $.each(products, function(){
+          console.log(this);
+        });
+      },
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      getDiscountPrice: function(products) {
+        var total = 0;
+        $.each(products, function(){
+          total += this.quantity * this.price;
+        });
+        return total * 1;
+      }
+    });
+
+  });
+  </script>
+
+  
+</body>
+</html>
