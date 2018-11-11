854f172c5ec7d3cbb533d2fa4210296205b26ea2
EMILYVTHOMAS1 <emilyvthomas.evt@gmail.com>
2018-11-11 13:53:38 +0530

    Revert Changes.

--- shop_admin/user_request.php
+++ shop_admin/user_request.php
@@ -329,7 +329,7 @@ include 'menu.php';
 		</div>
 		<!--footer-->
 		<div class="footer">
-		   <p>&copy; 2018 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts</a></p>
+		  
 	   </div>
         <!--//footer-->
 	</div>
