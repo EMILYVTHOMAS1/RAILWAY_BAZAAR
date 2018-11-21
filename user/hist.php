536dc35336cc9f36c29a7bd44d8dc89fe124c6f8
chaithanya5991 <42314356+chaithanya5991@users.noreply.github.com>
2018-11-21 19:20:46 +0530

    Updated track purchase for user module
    Track purchase for user has been successfully updated.

--- user/hist.php
+++ user/hist.php
@@ -307,37 +307,37 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<div class="col-md-3 footer-grid ">
 			<h3>Menu</h3>
 			<ul>
-				<li><a href="../temp/index.html">Home</a></li>
-				<li><a href="../temp/kitchen.html">Kitchen</a></li>
-				<li><a href="../temp/care.html">Personal Care</a></li>
-				<li><a href="../temp/hold.html">Household</a></li>						 
-				<li><a href="../temp/codes.html">Short Codes</a></li> 
-				<li><a href="../temp/contact.html">Contact</a></li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Kitchen</a></li>
+				<li><a href="#">Personal Care</a></li>
+				<li><a href="#">Household</a></li>						 
+				<li><a href="#">Short Codes</a></li> 
+				<li><a href="#">Contact</a></li>
 			</ul>
 		</div>
 		<div class="col-md-3 footer-grid ">
 			<h3>Customer Services</h3>
 			<ul>
-				<li><a href="../temp/shipping.html">Shipping</a></li>
-				<li><a href="../temp/terms.html">Terms & Conditions</a></li>
-				<li><a href="../temp/faqs.html">Faqs</a></li>
-				<li><a href="../temp/contact.html">Contact</a></li>
-				<li><a href="../temp/offer.html">Online Shopping</a></li>						 
+				<li><a href="#">Shipping</a></li>
+				<li><a href="#">Terms & Conditions</a></li>
+				<li><a href="#">Faqs</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Online Shopping</a></li>						 
 				 
 			</ul>
 		</div>
 		<div class="col-md-3 footer-grid">
 			<h3>My Account</h3>
 			<ul>
-				<li><a href="../temp/login.html">Login</a></li>
-				<li><a href="../temp/register.html">Register</a></li>
-				<li><a href="../temp/wishlist.html">Wishlist</a></li>
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Register</a></li>
+				<li><a href="#">Wishlist</a></li>
 				
 			</ul>
 		</div>
 		<div class="clearfix"></div>
 			<div class="footer-bottom">
-				<h2 ><a href="../temp/index.html"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
+				<h2 ><a href="../temp/index.html"><b>T<br>H<br>E</b>Railway Bazaar<span>The Best Supermarket</span></a></h2>
 				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
 				<ul class="social-fo">
 					<li><a href="../temp/#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
